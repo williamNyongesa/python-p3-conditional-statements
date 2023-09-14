d239749f0a566db2cfcd9f6a3332136ee7c66e5e
William Nyongesa <marakawilliam@gmail.com>
2023-09-14 13:07:11 +0300

    completed assignment

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,48 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
+    if username.lower() == "admin" and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
     # your code here
-    pass
+    
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        response = "brisk"
+    elif temperature >= 40 and temperature <= 65:
+        response = "a little chilly"
+    elif temperature > 85:
+        response = "too dang hot"
+    else:
+        response = "perfect"
+    return f"It's {response} out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 ==0 and num % 5 ==0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+fizzbuzz(3)
 
 def calculator(operation, num1, num2):
+    if operation == "/":
+        return num1 / num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "+":
+        return num1 + num2
+    else:
+        print("Invalid operation!")
+        return None
+        
+
     # your code here
-    pass
+    
